bf92b46db9f3eea98bae6f3214358a9fce83504d
reipand <113996356+reipand@users.noreply.github.com>
2023-12-04 19:14:20 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Your JavaScript code goes here
+
+// Example: Log a message to the console when the page is loaded
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Page loaded!');
+});
+
+// Add more JavaScript code as needed
