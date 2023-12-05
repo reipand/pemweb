1454c78f0002a4dcaecc3aa072a42e2bd013a701
reipand <113996356+reipand@users.noreply.github.com>
2023-12-05 13:08:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-// Your JavaScript code goes here
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
 
-// Example: Log a message to the console when the page is loaded
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
+
 document.addEventListener('DOMContentLoaded', function() {
     console.log('Page loaded!');
 });
